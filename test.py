70d7129090e2ea3154275b215e99a2569bcae324
fbagire <faibagire@gmail.com>
2022-08-15 20:35:14 +0200

    week2

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-# from cleantext import clean
-#
-# s = "¿Pos de cuál \;i-       orégano le puso al pozole, oiga? 🤔 Esa clase de viajes no los tenía ni Bob Marley. 🙊🐅"
-#
-# print(clean(s, no_emoji=True))
-import pygsheets
+from cleantext import clean
+
+s = "¿Pos de cuál \;i- \n     %!$ orégano le puso al pozole, oiga? 🤔 Esa clase de viajes no los tenía ni Bob Marley. 🙊🐅"
+
+print(clean(s, no_emoji=True, no_punct=True, no_line_breaks=True))
+# import pygsheets
 # gc = pygsheets.authorize()
 
-gc = pygsheets.authorize(service_file='tweet-auto-01-833e318c05c8.json')
+# gc = pygsheets.authorize(service_file='tweet-auto-01-833e318c05c8.json')
 
 # sh = gc.open('hh_test')
